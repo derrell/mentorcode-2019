790a98b36626cc2bd99100eaca5bf49747411143
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2019-05-21 19:38:14 -0400

    Robot moves autonomously!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrainSubsystem;
 
 import org.lipman.MessageBus.*;
 
@@ -26,7 +26,7 @@ import org.lipman.MessageBus.*;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+  public static DriveTrainSubsystem m_subsystem = new DriveTrainSubsystem();
   public static OI m_oi;
 
   Command m_autonomousCommand;
---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,113 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Config;
+
+/**
+ * An example subsystem. You can replace me with your own Subsystem.
+ */
+public class DriveTrainSubsystem extends Subsystem {
+  double speed = 0.0;
+  private CANSparkMax sparkFrontLeft;
+  private CANSparkMax sparkFrontRight;
+  private CANSparkMax sparkRearLeft;
+  private CANSparkMax sparkRearRight;
+  private DifferentialDrive drive;
+  private CANEncoder encoderFrontLeft;
+  private CANEncoder encoderFrontRight;
+  private CANEncoder encoderRearLeft;
+  private CANEncoder encoderRearRight;
+
+  public DriveTrainSubsystem() {
+    super("DriveTrainSubsystem");
+
+    int canFrontLeft;
+    int canRearLeft;
+    int canFrontRight;
+    int canRearRight;
+    Config config;
+    SpeedControllerGroup scgLeft;
+    SpeedControllerGroup scgRight;
+    
+
+    // Get a reference to the configuration
+    config = Config.getInstance();
+
+    // Set the default configuration for this subsystem
+    config.setDefaultInt("drivetrain:can:motor:frontleft", 3);
+    config.setDefaultInt("drivetrain:can:motor:frontright", 1);
+    config.setDefaultInt("drivetrain:can:motor:rearleft", 4);
+    config.setDefaultInt("drivetrain:can:motor:rearright", 2);
+    config.setDefaultString("drivetrain:drivetype", "arcade");
+
+    // Retrieve the canbus motor controller values
+    canFrontLeft = config.getInt("drivetrain:can:motor:frontleft");
+    canFrontRight = config.getInt("drivetrain:can:motor:frontright");
+    canRearLeft = config.getInt("drivetrain:can:motor:rearleft");
+    canRearRight = config.getInt("drivetrain:can:motor:rearright");
+
+    // Instantiate the motor controllers
+    sparkFrontLeft = new CANSparkMax(canFrontLeft, MotorType.kBrushless);
+    sparkFrontRight = new CANSparkMax(canFrontRight, MotorType.kBrushless);
+    sparkRearLeft = new CANSparkMax(canRearLeft, MotorType.kBrushless);
+    sparkRearRight = new CANSparkMax(canRearRight, MotorType.kBrushless);
+
+    // Left and right motors get their own unique speed controller groups, so they oeprate as a pair
+    scgLeft = new SpeedControllerGroup(sparkFrontLeft, sparkRearLeft);
+    scgRight = new SpeedControllerGroup(sparkFrontRight, sparkRearRight);
+
+    // Get access to each motor's encoder
+    encoderFrontLeft = sparkFrontLeft.getEncoder();
+    encoderFrontRight = sparkFrontRight.getEncoder();
+    encoderRearLeft = sparkRearLeft.getEncoder();
+    encoderRearRight = sparkRearRight.getEncoder();
+
+    // Get the high-level abstraction that provides us with various driving capabilities
+    drive = new DifferentialDrive(scgLeft, scgRight);
+  }
+
+  public void drive(double speed, double distance)
+  {
+    // Clamp the speed to legal values
+    if (speed < -1.0) speed = -1.0;
+    if (speed > 1.0)  speed = 1.0;
+
+    // Save the speed
+    this.speed = speed;
+ }
+
+  public void periodic()
+  {
+   // Drive straight ahead (forward or backwards) at the specified speed.
+    drive.arcadeDrive(speed, 0.0);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    Config config;
+
+    // Get a reference to the configuration
+    config = Config.getInstance();
+
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    
+    int setpoint = config.getInt("example:setpoint");
+
+    drive(0.4, 10.0);
+  }
+}
+
